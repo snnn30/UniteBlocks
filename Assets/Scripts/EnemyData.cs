17bd411f5c4194683e37d0e58ad9d8e25887d456
snnn30 <keikam82a1@outlook.jp>
2024-12-24 00:19:04 +0900

    多分没になるけど敵の攻撃システム実装

---
+++ Assets/Scripts/EnemyData.cs
@@ -0,0 +1,19 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObject/EnemyData")]
+public class EnemyData : ScriptableObject
+{
+    [SerializeField] int hp;
+    [SerializeField] int atk;
+    [SerializeField] float atkTime;
+    [SerializeField] Sprite sprite;
+
+    public int Hp => hp;
+    public int Atk => atk;
+    public float AtkTime => atkTime;
+    public Sprite Sprite => sprite;
+}
